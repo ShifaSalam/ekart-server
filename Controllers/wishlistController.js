c5bb69cd627703847bd62c691e8a3333ba82683d
Fathima <shifasalam036@gmail.com>
2024-06-13 18:59:26 +0530

    wishlist

--- Controllers/wishlistController.js
+++ Controllers/wishlistController.js
@@ -22,3 +22,27 @@ exports.addToWishlist=async (req,res)=>{
         res.status(401).json(err)
     }
 }
+
+exports.getWishList=async(req,res)=>{
+    try{
+        const userId=req.payload
+        const result=await wishlists.find({userId})
+        res.status(200).json(result)
+    }
+    catch(err){
+        console.log(err)
+        res.status(401).json(err)
+    }
+}
+
+exports.removeWish=async(req,res)=>{
+    try{
+        const wid=req.params.id
+        const result=await wishlists.findByIdAndDelete({_id:wid})
+        res.status(200).json("item removed!")
+    }
+    catch(err){
+        console.log(err)
+        res.status(401).json(err)
+    }
+}
--- Routes/router.js
+++ Routes/router.js
@@ -1,7 +1,6 @@
 const express=require('express')
 const productController=require('../Controllers/productController')
 const userController=require('../Controllers/userController')
-// const { addToWishlist } = require('../Controllers/wishlistController')
 const jwtMiddle=require('../Middlewares/jwtMiddleware')
 const wishController=require('../Controllers/wishlistController')
 
@@ -9,8 +8,12 @@ const router=new express.Router()
 
 router.get('/all-products',productController.allProducts)
 router.get('/single-product/:id',productController.singleProduct)
+
 router.post('/register',userController.userRegister)
 router.post('/login',userController.userLogin)
+
 router.post('/addwish',jwtMiddle,wishController.addToWishlist)
+router.get('/getwish',jwtMiddle,wishController.getWishList)
+router.delete('/removewish/:id',jwtMiddle,wishController.removeWish)
 
 module.exports=router
